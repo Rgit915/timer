e8bc36e4874553ce1549b8747af2fb99c7ef55bb
Rora Alem <rora.goitom@gmail.com>
2023-06-11 16:12:49 -0400

    Initial commit

---
+++ timer1.js
@@ -0,0 +1,43 @@
+//Implementation of Alaram clock/timer
+/*
+ - 'beep' sound  after specified time passed
+ -  input(in seconds) from user(command line arguments)
+ -  Example beep @ 3, 5, 9, 10, 15 seconds
+ -  using 'process.stdout.write('\x07');' perform a system sound i.e beep/Ding
+
+Edge cases:
+1. No numbers are provided: Easy.
+  - It should just not beep at all and end immediately since no beeps should get scheduled.
+2. An input is a negative number:
+  - Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+3. An input is not a number:
+  - Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+*/
+
+//Function validInput() to remove any invalid values such as negative and non numberic values
+
+const validInput = function (args) {
+
+  const times = [];  // to store the valid times
+  for (let i = 0; i < args.length; i++){
+    // convert the commandLine args(strings) into number
+    const time = Number(args[i]);
+    
+    if(!isNaN(time) && time >= 0){
+      times.push(time);
+    }
+  }
+  return times;
+
+};
+
+
+//to get command line arguments and using slice(2) excluding the first two elements
+
+const commandLineArgs = process.argv.slice(2);
+
+validInput(commandLineArgs);  // call the function
+
+
+
+
